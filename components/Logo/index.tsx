24bfb2ec1dd8f6bf422149da12baaba55ff582c8
anton-karlovskiy <antonkarlovskiy@outlook.com>
2023-03-18 04:26:43 +0100

    feat: add a title

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+
 import CustomRouterLink, { Props as CustomRouterLinkProps } from 'components/UI/CustomRouterLink';
 import { PAGES } from 'utils/constants/links';
 import LogoIcon from 'assets/images/logo.svg';
@@ -7,11 +9,17 @@ type Props = Omit<CustomRouterLinkProps, 'href' | 'children'>;
 const Logo = (props: Props) => {
   return (
     <CustomRouterLink
+      className={clsx(
+        'flex',
+        'items-center',
+        'space-x-4'
+      )}
       href={PAGES.HOME}
       {...props}>
       <LogoIcon
         width={58}
         height={50} />
+      <h2 className='text-2xl font-bold text-white'>Crosslink Protocol</h2>
     </CustomRouterLink>
   );
 };
