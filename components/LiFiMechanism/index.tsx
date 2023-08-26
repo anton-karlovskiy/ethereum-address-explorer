07f796811a794820e1eaaa46228354232b52d23b
A.K <antonkarlovskiy@outlook.com>
2023-08-27 01:53:51 +0900

    chore: clean up

--- components/LiFiMechanism/index.tsx
+++ components/LiFiMechanism/index.tsx
@@ -48,9 +48,9 @@ const LiFiMechanism = () => {
         //     }
         //   ]
         // }, // MEMO: display for a launch show
-        appearance: 'light', // MEMO: update to auto for a launch show
-        disableAppearance: true, // MEMO: display for a launch show
-        disableI18n: true, // MEMO: display for a launch show
+        appearance: 'light', // MEMO: display for a launch show (update to auto for a launch show)
+        // disableAppearance: true, // MEMO: display for a launch show
+        // disableI18n: true, // MEMO: display for a launch show
         languages: {
           default: 'en',
           allow: ['en']
